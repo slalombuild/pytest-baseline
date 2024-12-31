173794e86c369a0893e288e9db71243f6e57207b
Keith Knapp <knapp.keith@gmail.com>
2024-12-30 17:19:14 -0700

    your annoying:

--- tests/test_html_reporter.py
+++ tests/test_html_reporter.py
@@ -12,12 +12,14 @@ def run(
     path = testdir.tmpdir.join(path)
     result = testdir.runpytest("--html", path, *args)
     new_path = get_new_report_path(result, testdir)
+    print(new_path)
     try:
         read_file(new_path)
     except Exception:
         import os
-        print(", ".join([x for x in os.listdir(str(testdir.tmpdir)) if os.path.isfile(os.path.join(str(testdir.tmpdir), x))]))
         print("PROBLEM", result.stdout)
+    print("PROBLEM ")
+    print(", ".join([x for x in os.listdir(str(testdir.tmpdir)) if os.path.isfile(os.path.join(str(testdir.tmpdir), x))]))
 
     return result, read_file(new_path)
 
